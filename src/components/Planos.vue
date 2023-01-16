f07c57f4a29cca8993b4eac2bda85baa776bce86
Shalon Pereira dos Santos <shalon.santos@odontosystem.com.br>
2023-01-16 13:39:01 -0300

    melhando componente planos

--- src/components/Planos.vue
+++ src/components/Planos.vue
@@ -3,9 +3,8 @@
     <div class="container marketing">
       <div class="row cards">
         <div class="card">
-          <div class="fundo">
-          </div>
           <div class="card-body">
+            <img src="../assets/coracao.svg" alt="">
             <h1>Produto 1</h1>
             <p class="card-text">Algum texto de exemplo rápido para criar no título do cartão e compor a maior parte do conteúdo do cartão.
             </p>
@@ -13,9 +12,8 @@
           </div>
         </div>
         <div class="card">
-          <div class="fundo">
-          </div>
           <div class="card-body">
+            <img src="../assets/coracao.svg" alt="">
             <h1>Produto 2</h1>
             <p class="card-text">Algum texto de exemplo rápido para criar no título do cartão e compor a maior parte do conteúdo do cartão.
             </p>
@@ -23,9 +21,8 @@
           </div>
         </div>
         <div class="card">
-          <div class="fundo">
-          </div>
           <div class="card-body">
+            <img src="../assets/coracao.svg" alt="">
             <h1>Produto 3</h1>
             <p class="card-text">Algum texto de exemplo rápido para criar no título do cartão e compor a maior parte do conteúdo do cartão.
             </p>
@@ -33,9 +30,8 @@
           </div>
         </div>
         <div class="card">
-          <div class="fundo">
-          </div>
           <div class="card-body">
+            <img src="../assets/coracao.svg" alt="">
             <h1>Produto 4</h1>
             <p class="card-text">Algum texto de exemplo rápido para criar no título do cartão e compor a maior parte do conteúdo do cartão.
             </p>
@@ -49,23 +45,27 @@
 </template>
 
 <style scoped>
-img{
-    width: 5rem;
-}
-.fundo{
-  display: flex;
-  background: #4FC8EC;
-  height: 10rem;
-}
 .cards{
   display: flex;
   justify-content: space-around;
 }
+.card::before{
+  content: '';
+  position: absolute;
+  width: 150%;
+  height: 50%;
+  top: -5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 70%;
+  background: #37C6F4;
+}
 .card{
   overflow: hidden;
+  position: relative;
   max-width: 15rem;
-  border: 0.1rem solid rgba(206, 206, 206, 0.382);
-  padding: 0;
+  border: 0.1rem solid rgba(172, 172, 172, 0.382);
+  padding-bottom: 2rem;
 
   border-radius: 0.5rem;
   text-align: center;
@@ -77,5 +77,15 @@ img{
 .card img{
   margin-left: auto;
   margin-right: auto;
+}
+.card-body p{
+  text-align: justify;
+}
+.card-body img{
+  width: 3rem;
+  margin-bottom: 3rem;
+}
+.card-body{
+  z-index: 2;
 }
 </style>
